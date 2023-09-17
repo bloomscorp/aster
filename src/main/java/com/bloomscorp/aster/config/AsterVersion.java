fff8abd70d994d43f709057cef5dee12494635fd
Debabrata Acharya <11374405+onclave@users.noreply.github.com>
2023-09-18 01:00:19 +0530

    update dependencies

---
+++ src/main/java/com/bloomscorp/aster/config/AsterVersion.java
@@ -0,0 +1,7 @@
+package com.bloomscorp.aster.config;
+
+public class AsterVersion {
+
+	public static final String SPRING_BOOT_PARENT = "3.0.5";
+	public static final String BMX_SPRING_BASE = "0.0.40";
+}
