68666c62b08ce4a4748667695a8b792f791be75e
Debabrata Acharya <11374405+onclave@users.noreply.github.com>
2024-02-26 23:17:18 +0530

    feat:add AsterLogBookRepository under AsterApplication Imports

--- src/main/java/com/bloomscorp/aster/AsterApplication.java
+++ src/main/java/com/bloomscorp/aster/AsterApplication.java
@@ -1,6 +1,12 @@
 package com.bloomscorp.aster;
 
+import com.bloomscorp.aster.alfred.AsterCronManager;
+import com.bloomscorp.aster.alfred.AsterLogBook;
+import com.bloomscorp.aster.alfred.dao.repository.AsterLogBookRepository;
+import com.bloomscorp.aster.configuration.AsterConfiguration;
+import com.bloomscorp.aster.configuration.AsterLaunchSequence1;
 import com.bloomscorp.bsb.BmxApplication;
+import org.springframework.context.annotation.Import;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -10,6 +16,12 @@ import java.lang.annotation.Target;
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @BmxApplication
-@EnableAster
+@Import({
+	AsterConfiguration.class,
+	AsterLaunchSequence1.class,
+	AsterLogBook.class,
+	AsterCronManager.class,
+	AsterLogBookRepository.class
+})
 public @interface AsterApplication {
 }
