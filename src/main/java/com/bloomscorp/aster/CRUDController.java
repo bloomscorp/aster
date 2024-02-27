95d1b7c47f0b55d88a714bfcecc3f88472ab808c
Debabrata Acharya <11374405+onclave@users.noreply.github.com>
2024-02-27 20:32:09 +0530

    test:add TestLogBook

--- src/main/java/com/bloomscorp/aster/CRUDController.java
+++ src/main/java/com/bloomscorp/aster/CRUDController.java
@@ -14,29 +14,31 @@ import com.bloomscorp.nverse.NVerseGatekeeper;
 import com.bloomscorp.nverse.sanitizer.HttpRequestDumpSanitizer;
 import com.bloomscorp.raintree.RainTree;
 
-public abstract class CRUDController extends BehemothCRUDController<
-	AsterLogBook,
-	AsterLog,
-	AsterAuthenticationLog,
-	TenantFacade,
-	USER_ROLE,
-	AsterUserRole<USER_ROLE>
-> {
-	public CRUDController(
-		RainTree rainTree,
-		AsterLogBook logBook,
-		AsterCronManager cron,
-		NVerseGatekeeper<TenantFacade, USER_ROLE, UserRole> gatekeeper,
-		NVerseAuthorityResolver<LoomTenant, USER_ROLE, UserRole> authorityResolver,
-		HttpRequestDumpSanitizer httpRequestDumpSanitizer
-	) {
-		super(
-			rainTree,
-			logBook,
-			cron,
-			gatekeeper,
-			authorityResolver,
-			httpRequestDumpSanitizer
-		);
-	}
-}
+public class CRUDController {}
+
+//public abstract class CRUDController extends BehemothCRUDController<
+//	AsterLogBook,
+//	AsterLog,
+//	AsterAuthenticationLog,
+//	TenantFacade,
+//	USER_ROLE,
+//	AsterUserRole<USER_ROLE>
+//> {
+//	public CRUDController(
+//		RainTree rainTree,
+//		AsterLogBook logBook,
+//		AsterCronManager cron,
+//		NVerseGatekeeper<TenantFacade, USER_ROLE, UserRole> gatekeeper,
+//		NVerseAuthorityResolver<LoomTenant, USER_ROLE, UserRole> authorityResolver,
+//		HttpRequestDumpSanitizer httpRequestDumpSanitizer
+//	) {
+//		super(
+//			rainTree,
+//			logBook,
+//			cron,
+//			gatekeeper,
+//			authorityResolver,
+//			httpRequestDumpSanitizer
+//		);
+//	}
+//}
---
+++ src/main/java/com/bloomscorp/aster/alfred/TestLogBook.java
@@ -0,0 +1,60 @@
+package com.bloomscorp.aster.alfred;
+
+import com.bloomscorp.alfred.LogBook;
+import com.bloomscorp.alfred.adapter.ILogBookDAO;
+import com.bloomscorp.alfred.orm.AUTH_ACTION_ENUM;
+import com.bloomscorp.alfred.orm.LOG_TYPE;
+import com.bloomscorp.aster.alfred.orm.AsterAuthenticationLog;
+import com.bloomscorp.aster.alfred.orm.AsterLog;
+import com.bloomscorp.aster.tenant.orm.AsterUserRole;
+import com.bloomscorp.aster.tenant.orm.TenantFacade;
+import com.bloomscorp.aster.tenant.orm.USER_ROLE;
+import com.bloomscorp.nverse.pojo.NVerseRole;
+import com.bloomscorp.nverse.pojo.NVerseTenant;
+import com.bloomscorp.pastebox.Pastebox;
+
+public abstract class TestLogBook<
+	T extends NVerseTenant<E, R>,
+	E extends Enum<E>,
+	R extends NVerseRole<E>
+> extends LogBook<
+	AsterLog,
+	AsterAuthenticationLog,
+	T,
+	E,
+	R
+	> {
+
+	public TestLogBook(ILogBookDAO<AsterAuthenticationLog, AsterLog> repository) {
+		super(repository);
+	}
+
+	@Override
+	public AsterLog buildLogInstance(
+		String logger,
+		LOG_TYPE logType,
+		String message,
+		String dataDump
+	) {
+		return AsterLog
+			.builder()
+			.logger(logger)
+			.logType(logType)
+			.message(message)
+			.dataDump(dataDump)
+			.time(Pastebox.getCurrentTimeInMillis())
+			.build();
+	}
+
+//	@Override
+//	public AsterAuthenticationLog buildAuthenticationLogInstance(
+//		AUTH_ACTION_ENUM action,
+//		TenantFacade tenantFacade
+//	) {
+//		return AsterAuthenticationLog
+//			.builder()
+//			.action(action)
+//			.tenant(tenantFacade)
+//			.build();
+//	}
+}
