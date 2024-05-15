cd8cd277009f26453cc6aed3f88f942369538277
Debabrata Acharya <11374405+onclave@users.noreply.github.com>
2024-05-15 13:06:05 +0530

    feat: add @ConditionalMissingBean on AsterConfiguration

--- src/main/java/com/bloomscorp/aster/configuration/AsterConfiguration.java
+++ src/main/java/com/bloomscorp/aster/configuration/AsterConfiguration.java
@@ -3,6 +3,7 @@ package com.bloomscorp.aster.configuration;
 import com.bloomscorp.nverse.sanitizer.HttpRequestDumpSanitizer;
 import com.bloomscorp.raintree.RainTree;
 import org.apache.tika.Tika;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,16 +11,19 @@ import org.springframework.context.annotation.Configuration;
 public class AsterConfiguration {
 
 	@Bean
+	@ConditionalOnMissingBean
 	public RainTree rainTree() {
 		return new RainTree();
 	}
 
 	@Bean
+	@ConditionalOnMissingBean
 	public HttpRequestDumpSanitizer httpRequestDumpSanitizer() {
 		return new HttpRequestDumpSanitizer();
 	}
 
 	@Bean
+	@ConditionalOnMissingBean
 	public Tika tika() {
 		return new Tika();
 	}
