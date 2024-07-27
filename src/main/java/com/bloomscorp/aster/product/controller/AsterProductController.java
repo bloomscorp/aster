cd09d9b6eefa3710b00da290c874b82a2812f629
Debabrata Acharya <11374405+onclave@users.noreply.github.com>
2024-07-28 02:53:15 +0530

    feat: init AsterProductController

---
+++ src/main/java/com/bloomscorp/aster/product/controller/AsterProductController.java
@@ -0,0 +1,41 @@
+package com.bloomscorp.aster.product.controller;
+
+import com.bloomscorp.alfred.LogBook;
+import com.bloomscorp.alfred.cron.CronManager;
+import com.bloomscorp.alfred.orm.AuthenticationLog;
+import com.bloomscorp.aster.alfred.orm.AsterLog;
+import com.bloomscorp.aster.behemoth.AsterCRUDController;
+import com.bloomscorp.nverse.NVerseAuthorityResolver;
+import com.bloomscorp.nverse.NVerseGatekeeper;
+import com.bloomscorp.nverse.pojo.NVerseRole;
+import com.bloomscorp.nverse.pojo.NVerseTenant;
+import com.bloomscorp.nverse.sanitizer.HttpRequestDumpSanitizer;
+import com.bloomscorp.raintree.RainTree;
+
+public abstract class AsterProductController<
+	B extends LogBook<AsterLog, A, T, E, R>,
+	A extends AuthenticationLog,
+	T extends NVerseTenant<E, R>,
+	E extends Enum<E>,
+	R extends NVerseRole<E>,
+	C extends CronManager<B, AsterLog, A, T, E, R>
+> extends AsterCRUDController<B, A, T, E, R, C> {
+
+	public AsterProductController(
+		RainTree rainTree,
+		B logBook,
+		C cron,
+		NVerseGatekeeper<T, E, R> gatekeeper,
+		NVerseAuthorityResolver<T, E, R> authorityResolver,
+		HttpRequestDumpSanitizer httpRequestDumpSanitizer
+	) {
+		super(
+			rainTree,
+			logBook,
+			cron,
+			gatekeeper,
+			authorityResolver,
+			httpRequestDumpSanitizer
+		);
+	}
+}
